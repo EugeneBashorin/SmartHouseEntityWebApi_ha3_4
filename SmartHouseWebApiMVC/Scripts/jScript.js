d28c9f679e36a872d0e360ff7557affa16853a06
EugeneBashorin <bashorinjeka@gmail.com>
2017-06-26 12:18:09 +0300

    Changed JS, Removed JS from the Index page

--- SmartHouseWebApiMVC/Scripts/jScript.js
+++ SmartHouseWebApiMVC/Scripts/jScript.js
@@ -1,135 +1,138 @@
-﻿function sendSwitchParamAjax(id, command,devName) {
-    debugger;
-    $.ajax({
-        url: '/api/values/' + id + '/' + command,
-        type: 'PUT',
-        success: function (data) {
-            debugger;
-            var src;
-            if(command === "on")
-            {
-                src = "/Content/image/on" + devName + ".png"
-            }
-            else if (command === "off") {
-                src = "/Content/image/off" + devName + ".png"
-            }
-            $("#state-" + id).attr("src", src)
-            $("#conditionRow-"+id).html(data);
-        }
-    });
-}
+﻿//var devId = $("#Index-" + item.Id).val() 
+//var command = $("#command-" + id).val()
 
-//HandSetParams
-function sendSetParamAjax(id, command) {
-    debugger;
-    $.ajax({
-        url: '/api/values/' + id + '/' + command + '/' + $('#handSetPar-' + command + id).val(),
-        type: 'PUT',
-        success: function (data) {
-            debugger;
-            $("#conditionRow-" + id).html(data);
-        }
-    });
-}
-//DELETE
-function sendDelAjax(id) {
-    debugger;
-    $.ajax({
-        url: '/api/values/Delete/' + id,
-        type: 'DELETE',
-        success: function () {
-            $("#dev-" + id).remove();
-        }
-    });
-}
+function Action(id) {
+    var devName = $("#devName-" + id).val()
 
-
-/*
-function sendSwitchParamAjax(id, command) {
-    debugger;
-    $.ajax({
-        url: '/api/values/' + id + '/' + command,
-        type: 'PUT',
-        success: function (data) {
-            debugger;
+    $("#on-" + id).click(function (e) {
+        e.preventDefault();
+        debugger;
             $.ajax({
-                url: '/SmartHouse/RenderState' + $("#deviceName-" + id).val(),
-                type: 'POST',
-                data: JSON.stringify(data),
-                dataType: 'html',
-                contentType: 'application/json',
-                success: function (partial) {
+                url: '/api/values/' + id + '/on',
+                type: "PUT",
+                success: function (data) {
                     debugger;
-                    $("#result-" + id).html(partial);
+                    var src;
+                        src = "/Content/image/on" + devName + ".png"
+                    $("#state-" + id).attr("src", src)
+                    $("#conditionRow-" + id).html(data);
                 }
             });
-        }
     });
-}
-*/
-//HandSetParams
-//function sendSetParamAjax(id, command) {
-//    debugger;
-//    $.ajax({
-//        url: '/api/values/' + id + '/' + command + '/' + $('#handSetPar-' + command + id).val(),
-//        type: 'PUT',
-//        success: function (data) {
-//            debugger;
-//            $.ajax({
-//                url: '/SmartHouse/RenderState' + $("#deviceName-" + id).val(),
-//                type: 'POST',
-//                data: JSON.stringify(data),
-//                dataType: 'html',
-//                contentType: 'application/json',
-//                success: function (partial) {
-//                    debugger;
-//                    $("#result-" + id).html(partial);
-//                }
-//            });
-//        }
-//    });
-//}
+
+    $("#off-" + id).click(function (e) {
+        e.preventDefault();
+        debugger;
+        $.ajax({
+            url: '/api/values/' + id + '/off',
+            type: "PUT",
+            success: function (data) {
+                debugger;
+                var src;
+                    src = "/Content/image/off" + devName + ".png"
+                $("#state-" + id).attr("src", src)
+                $("#conditionRow-" + id).html(data);
+            }
+        });
+    });
+   
+    $("#incT-" + id).click(function (e) {
+        e.preventDefault();
+        debugger;
+        $.ajax({
+            url: '/api/values/' + id + '/IncTemp',
+            type: "PUT",
+            success: function (data) {
+                debugger;               
+                $("#conditionRow-" + id).html(data);
+            }
+        });
+    });
+
+    $("#decT-" + id).click(function (e) {
+        e.preventDefault();
+        debugger;
+        $.ajax({
+            url: '/api/values/' + id + '/DecTemp',
+            type: "PUT",
+            success: function (data) {
+                debugger;
+                $("#conditionRow-" + id).html(data);
+            }
+        });
+    });
+
+    //HandSetParams
+    $("#setParam-" + id).click(function (e) {
+        e.preventDefault();
+        var command = $("#InputCommand-" + id).val()
+        debugger;
+        $.ajax({
+            url: '/api/values/' + id + '/' + command + '/' + $('#handSetPar-' + command + id).val(),
+            type: 'PUT',
+            success: function (data) {
+                debugger;
+                $("#conditionRow-" + id).html(data);
+            }
+        });
+    });
+
+    //DELETE
+    $("#del-" + id).click(function (e) {
+        e.preventDefault();
+        debugger;
+        $.ajax({
+            url: '/api/values/Delete/' + id,
+            type: 'DELETE',
+            success: function () {
+                $("#dev-" + id).remove();
+            }
+        });
+    });
 
 
+   
+    
+    ////AC Mode
+    //function sendAjaxSetMode(id, command) {
+    //    //debugger;
+    //    $.ajax({
+    //        url: '/api/values/SetMode/' + $('#wMode').val(),
+    //        type: 'PUT',
+    //        success: function (result) {
+    //            //debugger;
+    //            $.ajax({
+    //                url: '/SmartHouse/RenderState' + $("#deviceName-" + id).val(),
+    //                type: 'POST',
+    //                contentType: 'application/html; charset=utf-8',
+    //                data: result,
+    //                success: function (partial) {
+    //                    $("#result-" + id).html(partial);
+    //                }
+    //            });
+    //        }
+    //    });
+    //}
 
-////AC Mode
-//function sendAjaxSetMode(id, command) {
-//    //debugger;
-//    $.ajax({
-//        url: '/api/values/SetMode/' + $('#wMode').val(),
-//        type: 'PUT',
-//        success: function (result) {
-//            //debugger;
-//            $.ajax({
-//                url: '/SmartHouse/RenderState' + $("#deviceName-" + id).val(),
-//                type: 'POST',
-//                contentType: 'application/html; charset=utf-8',
-//                data: result,
-//                success: function (partial) {
-//                    $("#result-" + id).html(partial);
-//                }
-//            });
-//        }
-//    });
-//}
+    //Bright MODE
+    //function sendAjaxSetBright(id) {
+    //    //debugger;
+    //    $.ajax({
+    //        url: '/api/values/SetBright/' + $("#brMode option:selected").text(),
+    //        type: 'PUT',
+    //        success: function (result) {
+    //            //debugger;
+    //            $.ajax({
+    //                url: '/SmartHouse/RenderState' + $("#deviceName-" + id).val(),
+    //                type: 'POST',
+    //                contentType: 'application/html; charset=utf-8',
+    //                data: result,
+    //                success: function (partial) {
+    //                    $("#result-" + id).html(partial);
+    //                }
+    //            });
+    //        }
+    //    });
+    //}
 
-//Bright MODE
-//function sendAjaxSetBright(id) {
-//    //debugger;
-//    $.ajax({
-//        url: '/api/values/SetBright/' + $("#brMode option:selected").text(),
-//        type: 'PUT',
-//        success: function (result) {
-//            //debugger;
-//            $.ajax({
-//                url: '/SmartHouse/RenderState' + $("#deviceName-" + id).val(),
-//                type: 'POST',
-//                contentType: 'application/html; charset=utf-8',
-//                data: result,
-//                success: function (partial) {
-//                    $("#result-" + id).html(partial);
-//                }
-//            });
-//        }
-//    });
-//}
+}//=>Action
